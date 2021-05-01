9eb3a06e4f21547aa3a1ee0267231589ff613c9a
jackyef <jacky.efendi1@gmail.com>
2021-05-01 15:37:18 +0700

    feat: Update redirect config on GitHub repo

--- src/pages/api/add-redirect.ts
+++ src/pages/api/add-redirect.ts
@@ -1,17 +1,28 @@
+import { updateRedirect } from '@/utils/github/updateRedirect'
 import { NowRequest, NowResponse } from '@now/node'
 
 export default async (req: NowRequest, res: NowResponse) => {
-  const url = req.query.url || ''
+  const url = String(req.query.url || '')
 
   if (url) {
-    // TODO: Update the `redirects` array in `vercel.json`
     try {
+      // check if it's a valid URL
+      // will throw an error if it's not
+      new URL(url)
+
+      const rerouteId = await updateRedirect(url)
+
       res.status(200)
-      res.json({ message: '' })
+      res.json({ rerouteId })
     } catch (err) {
-      res
-        .status(500)
-        .json({ message: '😱', error: err.message, stack: err.stack })
+      const debugInfo =
+        process.env.NODE_ENV === 'development'
+          ? {
+              error: err.message,
+              stack: err.stack,
+            }
+          : {}
+      res.status(500).json({ message: '😱', ...debugInfo })
     }
   } else {
     res.status(403).json({ message: '😠' })
